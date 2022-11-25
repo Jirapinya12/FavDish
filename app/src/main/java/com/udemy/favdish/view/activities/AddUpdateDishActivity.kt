21aae00478ea69f03db6197e53a93fed2dacb4e8
jirapinya eamsangchanchai <jirapinya.eamsangchanchai@scbtechx.io>
2022-11-25 18:12:44 +0700

    add custom dialog

--- app/src/main/java/com/udemy/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/udemy/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.udemy.favdish.view.activities
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.udemy.favdish.R
 import com.udemy.favdish.databinding.ActivityAddUpdateDishBinding
+import com.udemy.favdish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -32,14 +34,35 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         v?.apply {
             when (v.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(
-                        this@AddUpdateDishActivity,
-                        "You have clicked the ImageView",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this@AddUpdateDishActivity)
+
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        binding.llLayoutCamera.setOnClickListener {
+            Toast.makeText(
+                this@AddUpdateDishActivity,
+                "You have clicked on the Camera.",
+                Toast.LENGTH_SHORT
+            ).show()
+            dialog.dismiss()
+        }
+        binding.llLayoutGallery.setOnClickListener {
+            Toast.makeText(
+                this@AddUpdateDishActivity,
+                "You have clicked on the Gallery.",
+                Toast.LENGTH_SHORT
+            ).show()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
