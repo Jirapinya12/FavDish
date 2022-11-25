7e6f56b12c76b555beaf43659fbaa7daa7b2dbf5
jirapinya eamsangchanchai <jirapinya.eamsangchanchai@scbtechx.io>
2022-11-25 17:20:33 +0700

    handle on click iv_add_dish_image

--- app/src/main/java/com/udemy/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/udemy/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,10 +1,13 @@
 package com.udemy.favdish.view.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.udemy.favdish.R
 import com.udemy.favdish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
@@ -14,6 +17,7 @@ class AddUpdateDishActivity : AppCompatActivity() {
         setContentView(mBinding.root)
 
         setupActionBar()
+        mBinding.ivAddDishImage.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -21,6 +25,21 @@ class AddUpdateDishActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mBinding.toolbarAddDish.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        v?.apply {
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(
+                        this@AddUpdateDishActivity,
+                        "You have clicked the ImageView",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+            }
         }
     }
 }
