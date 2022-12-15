3bda2d701fbee1eda825c68b0ff4c582ce1f5f6c
jirapinya eamsangchanchai <jirapinya.eamsangchanchai@scbtechx.io>
2022-12-15 17:22:36 +0700

    V30 - Updating the entry using Room Database

--- app/src/main/java/com/udemy/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/udemy/favdish/view/fragments/DishDetailsFragment.kt
@@ -6,8 +6,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
@@ -16,7 +19,10 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.udemy.favdish.R
+import com.udemy.favdish.application.FavDishApplication
 import com.udemy.favdish.databinding.FragmentDishDetailsBinding
+import com.udemy.favdish.viewmodel.FavDishViewModel
+import com.udemy.favdish.viewmodel.FavDishViewModelFactory
 import java.io.IOException
 import java.util.*
 
@@ -24,6 +30,10 @@ class DishDetailsFragment : Fragment() {
 
 
     private var mBinding: FragmentDishDetailsBinding? = null
+
+    private val mFavDishViewModel: FavDishViewModel by viewModels {
+        FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -91,6 +101,46 @@ class DishDetailsFragment : Fragment() {
                         R.string.lbl_estimate_cooking_time,
                         it.dishDetails.cooking_time
                     )
+                if (args.dishDetails.favorite_dish) {
+                    mBinding?.ivFavoriteDish?.setImageDrawable(ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_selected
+                    ))
+                }
+                else {
+                    mBinding?.ivFavoriteDish?.setImageDrawable(ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_unselected
+                    ))
+                }
+            }
+        }
+
+        mBinding?.ivFavoriteDish?.setOnClickListener {
+            args.dishDetails.favorite_dish = !args.dishDetails.favorite_dish
+            mFavDishViewModel.update(args.dishDetails)
+
+            if (args.dishDetails.favorite_dish) {
+                mBinding?.ivFavoriteDish?.setImageDrawable(ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.ic_favorite_selected
+                ))
+                Toast.makeText(
+                    requireActivity(),
+                    resources.getString(R.string.msg_added_to_favorites),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else {
+                mBinding?.ivFavoriteDish?.setImageDrawable(ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.ic_favorite_unselected
+                ))
+                Toast.makeText(
+                    requireActivity(),
+                    resources.getString(R.string.msg_removed_from_favorite),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
